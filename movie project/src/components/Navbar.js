fe7d0c46444f7544003661c33e064a435b09e494
asta345 <81383112+asta345@users.noreply.github.com>
2021-10-28 23:41:05 +0530

    Add files via upload

--- movie project/src/components/Navbar.js
+++ movie project/src/components/Navbar.js
@@ -5,10 +5,11 @@ import React from 'react';
 class Navbar extends React.Component {
 	render(){
   return (
+
    <div className="nav">
 	   <div className="search-container">
 		   <input/>
-		   <button id="search-btn">Search</button>
+		   <button id="search-btn">{}Search</button>
 	   </div>
    </div>
   );
